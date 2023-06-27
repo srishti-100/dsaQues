0a27161fba167253a6f528e2fca1afeb84054c0a
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-06-27 22:08:56 +0530

    Time: 7 ms (35.72%), Space: 17.4 MB (11.04%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,20 +4,31 @@ public:
     
     void helper(int i, int n, vector<int>& arr,vector<int>v){
         if(i==n){
-            sort(v.begin(),v.end());
             ans.insert(v);
             return;
         }
+        
         helper(i+1,n,arr,v);
             
         v.push_back(arr[i]);
         helper(i+1,n,arr,v);        
         v.pop_back();
+       
+
+        // for(int j = i; j<n; j++){
+        //     if (j != i && arr[i] == arr[i - 1]) continue;
+        //     v.push_back(arr[i]);
+        //     helper(i+1,n,arr,v);        
+        //     v.pop_back();
+        //     helper(i+1,n,arr,v);
+
+        // }
         
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n = nums.size();
         vector<int>v;
+        sort(nums.begin(),nums.end());
         helper(0,n,nums,v);
         vector<vector<int>> res;
         for(auto i:ans){
