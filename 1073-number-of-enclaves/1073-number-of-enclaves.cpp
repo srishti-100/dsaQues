1225e4b78eba86510d590fff4f241eb7846bc890
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-07-04 21:06:29 +0530

    Time: 92 ms (30.62%), Space: 27.5 MB (81.68%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
     void helper(int r , int c, int rows, int cols ,vector<vector<int>>& grid, vector<vector<int>>& visited, int delRow[] , int delCol[]){
@@ -34,6 +36,7 @@ public:
             if(grid[i][m-1]==1 && visited[i][m-1]==-1){
                 helper(i,m-1,n,m,grid,visited,delRow, delCol);
             }
+
             if(grid[i][0]==1 && visited[i][0]==-1){
                 helper(i,0,n,m,grid,visited,delRow, delCol);
             }
@@ -49,5 +52,70 @@ public:
             }
         }
         return count;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,-1));
+        queue<pair<int,int>> q;
+
+        for(int i =0; i<m ; i++){
+            if(grid[0][i]==1 && visited[0][i]==-1){
+                q.push({0,i});
+                visited[0][i]=0;
+            }
+            if(grid[n-1][i]==1 && visited[n-1][i]==-1){
+                q.push({n-1,i});
+                visited[n-1][i]=0;
+            }
+        }
+
+        for(int i =0; i<n ; i++){
+            if(grid[i][m-1]==1 && visited[i][m-1]==-1){
+                q.push({i,m-1});
+                visited[i][m-1]=0;
+            }
+
+            if(grid[i][0]==1 && visited[i][0]==-1){
+                q.push({i,0});
+                visited[i][0]=0;
+            }
+        }
+
+        int delRow[]={-1,0,1,0};
+        int delCol[]={0,1,0,-1};
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i = 0; i<4; i++){
+                int nr = r + delRow[i];
+                int nc = c + delCol[i];
+
+                if(nr>=0 && nc>=0 && nr<n && nc<m && visited[nr][nc]==-1 && grid[nr][nc]==1){
+                    visited[nr][nc]=0;
+                    q.push({nr,nc});
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i =0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j]==1 && visited[i][j]==-1){
+                    count++;
+                }
+            }
+        }
+        return count;
+
     }
 };
