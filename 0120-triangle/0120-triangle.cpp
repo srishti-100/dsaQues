672985d74bcd2da40c28893eeda09f9ea9e4b872
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-08-02 17:49:13 +0530

    Time: 9 ms (27.87%), Space: 9 MB (18.87%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,26 +1,25 @@
 class Solution {
-public:
-    int mini = INT_MAX;
-    int helper(int r,int c, int rows, vector<vector<int>>& dp,vector<vector<int>>& mat){
-        if(c==rows){
+private:
+    int helper(int r, int c, int n, vector<vector<int>>& arr,vector<vector<int>>& dp){
+        if(r<0 || r>=n || c<0 || c>=r+1){
             return INT_MAX;
         }
-        if(r == rows-1) return mat[r][c];
         if(dp[r][c]!=-1) return dp[r][c];
-        int s1 = helper(r+1,c,rows,dp,mat);
-        int s2 = helper(r+1,c+1,rows,dp,mat);
-        dp[r][c] = min(s1,s2) + mat[r][c];
-        return dp[r][c];
-
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
         
-        int rows = triangle.size();
-        vector<vector<int>> dp;
-        for(int i =0; i<rows; i++){
-            vector<int>d(i+1,-1);
-            dp.push_back(d);
+        int s1 = helper(r+1,c,n,arr,dp);
+        int s2 = helper(r+1,c+1,n,arr,dp);
+
+        if(min(s1,s2)==INT_MAX){
+            dp[r][c] =  arr[r][c];
         }
-        return helper(0,0,rows,dp,triangle);
+        else dp[r][c] = min(s1,s2)+arr[r][c];
+        return dp[r][c];
+    }
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        int cols = triangle[n-1].size();
+        vector<vector<int>> dp(n,vector<int>(cols,-1));
+        return helper(0,0,n,triangle,dp);
     }
 };
