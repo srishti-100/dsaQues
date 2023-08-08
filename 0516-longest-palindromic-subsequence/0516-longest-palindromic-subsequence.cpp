e6258331db9a46a834b4310dd66499ca392c3540
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-08-08 07:27:19 +0530

    Time: 165 ms (54.97%), Space: 73 MB (55.08%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -36,6 +36,7 @@ public:
         return lmax;
     }
     */
+    /*
     private:
     int helper(int i, int j, int n, string& s, vector<vector<int>>& dp){
         if(i==n-1 || j==0) return 1;
@@ -61,7 +62,56 @@ public:
         int i =0;
         int n = s.length();
         int j = n-1;
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n,vector<int>(n,0));
         return helper(i,j,n,s,dp);
     }
+    */
+
+    public:
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+        reverse(t.begin(),t.end());
+        vector<vector<int>> dp(s.length()+1,vector<int>(s.length()+1,0));
+        for(int i =0; i<=s.length(); i++){
+            dp[i][0]=0;
+        }
+        for(int i =0; i<=s.length(); i++){
+            dp[0][i]=0;
+        }
+        
+        for(int i=1; i<=s.length(); i++){
+            for(int j=1; j<=s.length(); j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[s.length()][s.length()];
+        // string ans= "";
+        // int i =s.length();
+        // int j = s.length();
+
+        // while(i!=0 && j!=0){
+        //     if(s[i-1]==t[j-1]){
+        //         ans+=s[i];
+        //         i--;
+        //         j--;
+        //     }
+        //     else{
+        //         if(dp[i-1][j]>dp[i][j-1]){
+        //             i--;
+        //         }
+        //         else{
+        //             j--;
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+    }
+
 };
