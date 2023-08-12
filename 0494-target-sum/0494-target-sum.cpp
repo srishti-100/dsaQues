ad486b8ce3d4214a4dac059f604e734b44c29247
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-08-12 10:49:44 +0530

    Time: 1071 ms (20.32%), Space: 9.1 MB (74.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    /*
 private:
     void helper(int idx, int n, int target, vector<int>& nums, int&count){
         if(idx==n){
@@ -20,3 +21,25 @@ public:
         return count;
     }
 };
+*/
+private:
+    int helper(int idx, int n, int target, vector<int>& nums){
+        if(idx==n){
+            if(target==0){
+                return 1;
+            }
+            return 0;
+        }
+
+        int pos = helper(idx+1, n, target-nums[idx],nums);
+        int neg = helper(idx+1,n,target+nums[idx],nums);
+        return neg+pos;
+    }
+
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return helper(0,nums.size(), target,nums);
+        
+    }
+};
+
