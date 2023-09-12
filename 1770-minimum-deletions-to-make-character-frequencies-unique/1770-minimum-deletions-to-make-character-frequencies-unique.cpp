57fd88e01e563883609f156c5a8bbc82746c4db9
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-09-12 14:03:16 +0530

    Time: 74 ms (42.72%), Space: 17.5 MB (37.86%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,46 +1,21 @@
 #include<bits/stdc++.h>
 class Solution {
-private:
-    
 public:
-    static bool cmp(pair<char, int>&a,  pair<char, int>& b)
-        {
-            return a.second > b.second;
-        }
-    static void sortMap(unordered_map<char, int>& mp){
-        vector<pair<char, int> > A;
- 
-        for (auto& it : mp) {
-            A.push_back(it);
-        }
-    
-        sort(A.begin(), A.end(), cmp);
-        mp.clear();
-
-        for(auto it: A){
-            mp[it.first] = it.second; 
-        }
-    }
     int minDeletions(string s) {
-        vector<int> f(26,0);
+        unordered_map<char,int> mp;
         
         for(int i =0; i<s.length(); i++){
-            f[s[i]-'a']++;
+            mp[s[i]]++;
         } 
-
-        sort(f.begin(), f.end());
     
         set<int> freq;
-        int i = 25;
         int cnt=0;
-        while(i>=0 && f[i]!=0 ){
-            while(freq.find(f[i])!=freq.end()){
-                if(f[i]==0) break;
-                f[i]--;
+        for(auto it: mp){
+            while(it.second>0 && freq.find(it.second)!=freq.end()){
+                it.second--;
                 cnt++;
             }
-            freq.insert(f[i]);
-            i--;
+            freq.insert(it.second);
         }
 
         return cnt;
