755e885fe1eb3401c68ca93e947521c01787cd32
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-09-15 22:20:29 +0530

    Time: 3 ms (99.99%), Space: 24.3 MB (53.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -27,11 +27,12 @@ public:
 
         int n = nums.size();
         vector<int> ans(n,0);
-
-        ans[0] =1;
+        ans[0] = 1;
+        int left = nums[0];
         int right =  1;
         for(int i =1; i<n; i++){
-            ans[i] = ans[i-1]*nums[i-1];
+            ans[i] = left;
+            left = left*nums[i];
             
         }
 
