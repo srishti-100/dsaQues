45a4ccc652b22f264e458a57c10aa8c7377e4d10
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2023-09-20 19:31:40 +0530

    Time: 104 ms (53.14%), Space: 58.4 MB (44.76%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        /*
         sort(nums.begin(),nums.end());
 
         unordered_map<int,int> mp;
@@ -17,6 +18,28 @@ public:
                 cnt++;
             }
         }
+        return cnt;
+        */
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 0;
+        int j = n-1;
+        int cnt = 0;
+
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                i++;
+                j--;
+                cnt++;
+            }
+            else if(nums[i]+nums[j]>k){
+                j--;
+            }
+            else
+                i++;
+        }
+
         return cnt;
     }
 };
