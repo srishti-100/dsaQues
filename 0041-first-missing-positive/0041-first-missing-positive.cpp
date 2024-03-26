7b401dcaf1850394d1973fd6b16835ae76ed737f
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2024-03-26 10:08:32 +0530

    Time: 41 ms (71.13%), Space: 43.5 MB (81.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        /*
         unordered_set<int> st;
         // int mini = INT_MAX;
         int maxi = 0;
@@ -20,5 +21,16 @@ public:
             }
         }
         return maxi+1;
+        */
+        
+        
+        int n = nums.size(); 
+        for (int i = 0; i < n; i++)
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i] - 1]);
+        for (int i = 0; i < n; i++)
+            if (nums[i] != i + 1)
+                return i + 1;
+        return n + 1;
     }
 };
