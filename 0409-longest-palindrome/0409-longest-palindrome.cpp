f9bcd22919ed03d4cdc56295740c45c348420b9a
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2024-06-04 14:57:16 +0530

    Time: 3 ms (55.35%), Space: 9.4 MB (6.93%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int len = 0;
+        set<char> st;
+
+        for(int i =0; i<s.length(); i++){
+            if(st.find(s[i])==st.end()){
+                st.insert(s[i]);
+            }
+            else{
+               st.erase(s[i]);
+               len+=2;
+            }
+        }
+        if(st.empty()){
+            return len;
+        }
+        return len+1;
+    }
+};
