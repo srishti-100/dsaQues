e1a4ac3d9cce567875963a8ab0a6a857df8d0294
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2024-06-17 21:07:16 +0530

    Time: 3 ms (68%), Space: 6.9 MB (91.95%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
+        /*
         unordered_map<long, int> ump;
 
         for(int i=0; i<=sqrt(c); i++){
@@ -13,5 +14,21 @@ public:
         }
 
         return false;
+        */
+
+        for(int i = 2; i<=sqrt(c); i++){
+            if(c%i==0){
+                int count = 0;
+                while(c%i==0){
+                    count++;
+                    c=c/i;
+                }
+                if(i%4==3 && count%2!=0){
+                    return false;
+                }
+            }
+        }
+        return c%4 != 3;
+    
     }
 };
