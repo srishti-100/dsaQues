f7c3c2df81bb958e2b386f780205db49e8934da9
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2024-07-10 02:17:12 +0530

    Time: 35 ms (54.2%), Space: 18.6 MB (23.67%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i =0;
+        int j = s.length()-1;
+
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+                continue;
+            }
+            else{
+                char ch = ' ';
+
+                if(s[i]<s[j]){
+                    s[j] = s[i];
+                }
+                else{
+                    s[i]=s[j];
+                }
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
