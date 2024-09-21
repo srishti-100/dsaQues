2da670e5be397affba9ad0429e46dea0af71df6c
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2024-09-21 22:46:18 +0530

    Time: 434 ms (5.05%), Space: 17.1 MB (33.66%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    static bool compare(int& a, int& b){
+        string s = to_string(a);
+        string t = to_string(b);
+
+        return s<t;
+    }
     vector<int> lexicalOrder(int n) {
-        vector<string> vec;
+        // vector<string> vec;
+        // vector<int> ans;
+
+        // for(int i =1; i<=n; i++){
+        //     vec.push_back(to_string(i));
+        // }
+        // sort(vec.begin(), vec.end());
+
+        // for(int i =0; i<n; i++){
+        //     ans.push_back(stoi(vec[i]));
+        // }
+        // return ans;
         vector<int> ans;
-
         for(int i =1; i<=n; i++){
-            vec.push_back(to_string(i));
+            ans.push_back(i);
         }
-        sort(vec.begin(), vec.end());
 
-        for(int i =0; i<n; i++){
-            ans.push_back(stoi(vec[i]));
-        }
+        sort(ans.begin(), ans.end(), compare);
+
         return ans;
     }
 };
