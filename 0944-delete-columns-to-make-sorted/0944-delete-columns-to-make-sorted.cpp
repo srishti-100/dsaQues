ba0cb264a2fb48c2c7f7c5e49810980afc87f442
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2025-12-20 20:33:05 +0530

    Time: 1 ms (71.56%), Space: 15.4 MB (96.53%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -6,17 +6,25 @@ public:
         bool flag = true;
         int count = 0;
 
+        // for(int i = 0; i<cols; i++){
+        //     for(int j = 0; j<rows-1; j++){
+        //         if(strs[j][i] > strs[j+1][i]){
+        //             flag  = false;  
+        //             break;
+        //         }
+        //     }
+        //     if(flag == false){
+        //         count++;
+        //     }
+        //     flag = true;
+        // }
         for(int i = 0; i<cols; i++){
             for(int j = 0; j<rows-1; j++){
                 if(strs[j][i] > strs[j+1][i]){
-                    flag  = false;  
+                    count++;
                     break;
                 }
             }
-            if(flag == false){
-                count++;
-            }
-            flag = true;
         }
         return count;
     }
