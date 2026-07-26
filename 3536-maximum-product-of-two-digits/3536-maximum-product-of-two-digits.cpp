cc4a2f838b77fd7de41197c34ad2d5965d59bfe3
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2026-07-27 00:22:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.04%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int maxProduct(int n) {
+        int maxi = INT_MIN;
+        int secondMaxi = INT_MIN;
+
+        while(n!=0){
+            int rem = n%10;
+            if(rem>maxi){
+                secondMaxi = maxi;
+                maxi = rem;
+            }
+            else if(rem > secondMaxi){
+                secondMaxi = rem;
+            }
+        n/=10;
+        }
+
+        return maxi*secondMaxi;
+
+        /*
         vector<int> nums;
 
         while(n!=0){
@@ -13,5 +31,6 @@ public:
         sort(nums.begin(), nums.end());
 
         return nums[s-1] * nums[s-2];
+        */
     }
 };
