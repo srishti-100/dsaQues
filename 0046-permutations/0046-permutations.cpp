43ec64cbaecb1729286df50d5dac67216d6201af
srishti-100 <88020118+srishti-100@users.noreply.github.com>
2026-08-11 11:32:54 +0530

    Time: 4 ms (9.96%), Space: 11.9 MB (12.05%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,55 +1,30 @@
 class Solution {
 public:
-//Method 1
-/*
-    void helper(vector<int>& visited, vector<int>& r, vector<int>& nums, vector<vector<int>>& ans){
-        if(r.size()==nums.size()){
-            ans.push_back(r);
+    void helper(vector<bool>& visited, vector<int>&nums, vector<vector<int>>& ans, vector<int> temp){
+        if(temp.size()==nums.size()){
+            ans.push_back(temp);
             return;
         }
 
         for(int i =0; i<nums.size(); i++){
-            if(visited[i]==1) continue;
-            r.push_back(nums[i]);
-            visited[i]=1;
-            helper(visited, r, nums, ans);
-            visited[i]=0;
-            r.pop_back();
+            if(!visited[i]){
+                visited[i] = true;
+                temp.push_back(nums[i]);
+                helper(visited, nums, ans, temp);
+                temp.pop_back();
+                visited[i] = false;
+            }
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
-        vector<int> visited(n,0);
-        vector<int> r;
+        vector<bool> visited(n,false);
+
         vector<vector<int>> ans;
-        helper(visited,r, nums, ans);
-        return ans;
-    }
-    */
-    void swap(int&a, int& b){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
+        vector<int> temp;
 
-    void helper(int idx,vector<int>& nums, vector<vector<int>>& ans){
-        if(idx==nums.size()){
-            ans.push_back(nums);
-            return;
-        }
-        for(int i=idx;i<nums.size(); i++){
-            swap(nums[i],nums[idx]);
-            helper(idx+1, nums, ans);
-            swap(nums[i],nums[idx]);
-        }
-    }
-
-
-    vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        helper(0,nums, ans);
+        helper(visited, nums, ans,temp);
         return ans;
     }
 };
